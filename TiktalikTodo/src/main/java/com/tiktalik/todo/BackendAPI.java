7d57c31e0275aa90509bc2a62a37a1a6acc7fbcf
Aleksander Piotrowski <alek@nic.com.pl>
2013-10-11 16:26:49 +0200

    request internet access; new api endpoint

--- TiktalikTodo/src/main/java/com/tiktalik/todo/BackendAPI.java
+++ TiktalikTodo/src/main/java/com/tiktalik/todo/BackendAPI.java
@@ -11,9 +11,9 @@ import retrofit.http.POST;
  * Created by alek on 10/10/13.
  */
 public interface BackendAPI {
-    @POST("/item/")
+    @POST("/api/item")
     Response addItem(@Body Item item);
 
-    @GET("/item/")
+    @GET("/api/item")
     ArrayList<Item> getItems();
 }
